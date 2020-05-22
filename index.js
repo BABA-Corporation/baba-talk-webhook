821e3b23f0dc87cf4c2aac6107f2c8531db0d988
Anthony Ilhat <sliw.max@gmail.com>
2020-05-22 19:33:27 +0200

    Signin with Actions Google

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const {WebhookClient} = require('dialogflow-fulfillment');
 // const {Card, Suggestion} = require('dialogflow-fulfillment');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const {dialogflow} = require('actions-on-google');
+const {dialogflow, SignIn} = require('actions-on-google');
 const api = require('./ApiService');
+const askForSignIn = require('./AuthService');
 
 require('dotenv').config();
 
@@ -34,10 +35,14 @@ function normalizePort(val) {
     return false;
 }
 
-const port = normalizePort(process.env.PORT || 3000);
+const port = normalizePort(process.env.PORT || 5002);
 
 const server = express();
-// const assistant = dialogflow();
+const assistant = dialogflow({
+    clientId: process.env.CLIENT_ID,
+    debug: true,
+});
+
 
 process.env.DEBUG = 'dialogflow:debug';
 server.set('port', port);
@@ -59,7 +64,7 @@ server.get('/', (req, res) => {
     res.status(200).send('BABA TALK Webhook is working.')
 });
 
-async function getArticle(agent) {
+async function test(agent) {
     const possibleResponse = [
         'Toute la data que tu veux frèroo !',
         'Je te sors toutes les infos mec',
@@ -79,29 +84,70 @@ async function getArticle(agent) {
     const apiUrl = server.get('api');
     const user = await api.getUserByMail("jean-mich@baba.click", "P@ssword1!", apiUrl);
 
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         axios.get(apiUrl + "/api/private/user/" + user.userId + '/articles', {
             headers: {
                 Authorization: `Bearer ${user.token}`
             }
         })
-        .then(function (res) {
-            const parseData = res.data;
-            const articles = parseData[0]['Bitcoins,Apple,Android'];
-            const article = articles[0];
-            let outputMsg = 'Voici un article de jean mich : ';
-            outputMsg += article.title;
-            let output = agent.add(outputMsg);
-            resolve(output);
+            .then(function (res) {
+                const parseData = res.data;
+                const articles = parseData[0]['Bitcoins,Apple,Android'];
+                const article = articles[0];
+                let outputMsg = 'Voici un article de jean mich : ';
+                outputMsg += article.title;
+                let output = agent.add(outputMsg);
+                resolve(output);
+            })
+            .catch(function (error) {
+                console.log(error);
+                let output = 'Désolé, impossible de joindre l\'API';
+                resolve(output);
+            })
+            .then(function () {
+                // always executed
+            });
+    });
+}
+
+async function getVeille(agent) {
+    const possibleResponse = [
+        'Toute la data que tu veux frèroo !',
+        'Je te sors toutes les infos mec',
+        'Pas de soucis le sang de la veine'
+    ];
+
+    let pick = Math.floor(Math.random() * possibleResponse.length);
+
+    let response = possibleResponse[pick];
+    agent.add(response);
+
+    const apiUrl = server.get('api');
+    const user = await api.getUserByMail("jean-mich@baba.click", "P@ssword1!", apiUrl);
+
+    return new Promise((resolve) => {
+        axios.get(apiUrl + "/api/private/user/" + user.userId + '/articles', {
+            headers: {
+                Authorization: `Bearer ${user.token}`
+            }
         })
-        .catch(function (error) {
-            console.log(error);
-            let output = 'Désolé, impossible de joindre l\'API';
-            resolve(output);
-        })
-        .then(function () {
-            // always executed
-        });
+            .then(function (res) {
+                const parseData = res.data;
+                const articles = parseData[0]['Bitcoins,Apple,Android'];
+                const article = articles[0];
+                let outputMsg = 'Voici un article de jean mich : ';
+                outputMsg += article.title;
+                let output = agent.add(outputMsg);
+                resolve(output);
+            })
+            .catch(function (error) {
+                console.log(error);
+                let output = 'Désolé, impossible de joindre l\'API';
+                resolve(output);
+            })
+            .then(function () {
+                // always executed
+            });
     });
 }
 
@@ -112,7 +158,8 @@ function WebhookProcessing(req, res) {
     debug('Dialogflow Request body: ' + JSON.stringify(req.body));
 
     let intentMap = new Map();
-    intentMap.set('test', getArticle);
+    intentMap.set('test', test);
+    intentMap.set('Veille', getVeille);
 
     agent.handleRequest(intentMap).then(r => console.log(intentMap.size));
 }
@@ -122,7 +169,21 @@ server.post('/', function (req, res) {
     WebhookProcessing(req, res);
 });
 
+assistant.intent('Start Signin', (conv) => {
+    conv.ask(new SignIn('To get your account details'));
+});
+// Create a Dialogflow intent with the `actions_intent_SIGN_IN` event.
+assistant.intent('Get Signin', (conv, params, signin) => {
+    if (signin.status === 'OK') {
+        const payload = conv.user.profile.payload;
+        conv.ask(`I got your account details, ${payload.name}. What do you want to do next?`);
+    } else {
+        conv.ask(`I won't be able to save your data, but what do you want to do next?`);
+    }
+});
+
+server.post('/fulfillment', assistant);
 
 server.listen(server.get('port'), () => {
-    debug('Webhook is running at port ' + server.get('port'))
+    debug('Webhook is running at port ' + server.get('port') + ' with ' + process.env.NODE_ENV + ' env')
 });
